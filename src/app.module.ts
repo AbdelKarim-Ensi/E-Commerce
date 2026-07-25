758b019f40d02e90a5e04da766dc432f5f4a6f09
Abdel Karim Doudey <abdelkarim.doudey@ensi-uma.tn>
2026-07-25 12:42:12 +0100

    chore: wire RedisModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { validateEnv } from './config/env.validation';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { CategoriesModule } from './modules/categories/categories.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { validateEnv } from './config/env.validation';
+import { RedisModule } from './redis/redis.module';
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validate: validateEnv,
-    }),
+    ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
     PrismaModule,
+    AuthModule,
     ProductsModule,
+    CategoriesModule,
+    OrdersModule,
+    RedisModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
